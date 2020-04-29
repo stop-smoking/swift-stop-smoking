b80e75befdb35b8776379dc0fd4cfac90fd45b1d
Nicolas Barbosa <n.barbosa.pro@gmail.com>
2020-04-29 19:27:58 +0200

    initialization of project

---
+++ stop_smoking/Screens/Home/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stop_smoking
+//
+//  Created by Nicolas Barbosa on 29/04/2020.
+//  Copyright © 2020 Nicolas Barbosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
